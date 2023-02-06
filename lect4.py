dba844a60d078e2bcc84b7b8d1e0c68c24f5a97e
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-06 23:56:39 +0530

    intro to string function and string slicing

--- lect4.py
+++ lect4.py
@@ -14,3 +14,11 @@ print(mystr[-4:-2])# b
 print(mystr[::-1])#yob doog a si ivaR
 print(mystr[::-2])#ybdo  iia
 # it'll first reverse the string and then skips 2 letters
+print(mystr.isalnum())#False because of spaces in between
+print(mystr.isalpha())# false
+print(mystr.endswith("boy"))#true
+print(mystr.count("v"))#1
+print(mystr.capitalize())
+print(mystr.lower())#ravi is a good boy
+print(mystr.upper())#RAVI IS A GOOD BOY
+print(mystr.replace("is","are"))#Ravi are a good boy
