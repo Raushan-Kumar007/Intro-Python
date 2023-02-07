7670b35ca49fa6301e2f99ccca9cef476879b82b
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-08 00:24:21 +0530

    conditional statement

---
+++ lect8.py
@@ -0,0 +1,21 @@
+# var1 = 6
+# var2 = 56
+# var3 = int(input())
+# if var3>var2:
+#     print("greater")
+# elif var3==var2:
+#     print("equal")
+# else:
+#     print("lesser")
+# list1 = [5,7,6]
+# print(15 not in list1)
+# if 15 not in list1:
+#     print("Yes it's in list")
+print("Enter your Age")
+age = int(input())
+if age>18:
+    print("Yes you can drive")
+elif age==18:
+    print("Eligible Apply for DL")
+else:
+    print("No you cannot drive")
