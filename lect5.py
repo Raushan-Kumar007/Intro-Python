bfd345031369616b6f6601264ed7c0b33d2fe6cd
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-07 23:20:17 +0530

    intro to list and touples

---
+++ lect5.py
@@ -0,0 +1,39 @@
+student = ["Name","RegNo","Section","Course","Age",56];
+#print(student[5])
+numbers = [2,7,9,11,3]
+#numbers.sort()
+#numbers.reverse()
+#print(max(numbers))#11
+#print(min(numbers))#2
+#print(numbers[:])#[2, 7, 9, 11, 3]
+#print(numbers[1:])#[7, 9, 11, 3]
+#print(numbers[:5])#[2, 7, 9, 11, 3]
+#print(numbers[::])#[2, 7, 9, 11, 3], here bydefault third perameter one
+#print(numbers[::2])#[2, 9, 3]
+#print(numbers[::3])#[2, 11]
+#print(numbers[::-1])#[3, 11, 9, 7, 2]
+#print(numbers[::-2])
+#numbers=[]
+# numbers.append(11)
+# numbers.append(12)
+# numbers.append(13)
+#numbers.insert(1,67)
+#print(numbers)
+# numbers.remove(9)
+# print(numbers)
+#numbers.pop();
+#print(numbers)
+#mutable - can chanfe
+#immutable - cannot change
+# tp=(1,2,3)
+# print(tp)
+# tp=(1,)
+# print(tp)
+a=1
+b=8
+a,b=b,a
+# temp = a
+# a = b
+# b = temp
+print(a,b)
+
