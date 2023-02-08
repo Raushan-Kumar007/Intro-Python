315a3a6047c7c44bacaf054a10b718c68700cfbf
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-08 22:18:55 +0530

    intro to loop

--- lect9.py
+++ lect9.py
@@ -1,9 +1,14 @@
-list1 = [["Ravi",1],["Raushan",2],["Rahul",5],["Razz",24]]
+# list1 = [["Ravi",1],["Raushan",2],["Rahul",5],["Razz",24]]
 # for item in list1:
 #     print(item)
-dict1 = dict(list1)
+# dict1 = dict(list1)
 
-for item in dict1:
-    print(item)
+# for item in dict1:
+#     print(item)
 # for item,bread in dict1.items():
 #     print(item,bread)
+# +++++quize++++
+list1 = [int,float,"Ravi",12,3,5,7,11,6]
+for item in list1:
+    if str(item).isnumeric() and item>6:
+        print(item)
