e7a843efa10180cc2f7cd4fcbbb1d31b91a9c79e
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-08 23:29:44 +0530

    intro to operator

---
+++ lect12.py
@@ -0,0 +1,51 @@
+# comparison operator
+# Arthematic operator
+# logical operator
+# identity operator
+# membership operator
+# bitwise operator
+
+# Arthematic operator
+print("12 + 6 is ", 12 + 6)
+print("12 - 6 is ", 12 - 6)
+print("12 * 6 is ", 12 * 6)
+print("12 / 6 is ", 12 / 6)
+print("12 % 6 is ", 12 % 6)
+print("12 ** 6 is ", 12 ** 6)
+print("12 // 6 is ", 12 // 6)
+
+# Assignment operator
+x = 5
+print(x)
+x += 7 # x = x + 7
+print(x)
+
+# Comparision Operator
+i = 5
+print(i==5)
+print(i>5)
+print(i<5)
+
+# logical operator
+a = True
+b = False
+print(a and b)
+print(a or b)
+
+# identity operator
+a = True
+b = False
+print(a is b)
+print(a is not b)
+
+# membership operator
+list = [3,2,12,6,78,9]
+print(3  in list)#true
+
+# bitwise operator
+# 0 = 00
+# 1 - 01
+# 2 = 10
+# 3 = 11
+print(0 & 1)#0
+print(0 | 1)#1
