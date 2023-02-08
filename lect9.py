791278280743eb876b4a97611961ad4056030dba
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-08 22:10:51 +0530

    intro to loops

---
+++ lect9.py
@@ -0,0 +1,9 @@
+list1 = [["Ravi",1],["Raushan",2],["Rahul",5],["Razz",24]]
+# for item in list1:
+#     print(item)
+dict1 = dict(list1)
+
+for item in dict1:
+    print(item)
+# for item,bread in dict1.items():
+#     print(item,bread)
