aec1570f2499b859242e8b6fab240106e694d6bb
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-09 00:03:46 +0530

    intro to exception handlling

---
+++ lect14.py
@@ -0,0 +1,9 @@
+print("Enter the number1")
+num1 = input()
+print("Enter the number2")
+num2 = input()
+try:
+    print("sum of number1 and number2 is",int(num1)+int(num2))
+except Exception as e:
+    print(e)
+print("it's very imprtant")
