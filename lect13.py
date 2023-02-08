9a60caab2845c7002931d84a1b739325fc383c18
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-08 23:53:36 +0530

    intro to function and docString

---
+++ lect13.py
@@ -0,0 +1,18 @@
+# a = 9
+# b = 8
+# c = sum((a,b)) builtin function
+# print(c)
+# def function1():
+#     print("Hello you are in function 1")
+# function1()
+def function1(a,b):
+    print("hello you are in function 1", a+b)
+function1(7,5)
+def function2(a,b):
+    """this will find the average of number"""
+    avg = (a+b)/2
+    print(avg)
+    return avg
+# v = function2(5,7)
+# print(v)
+print(function2. __doc__)
