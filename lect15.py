def55ecebdd06b84edac77295674164c2053f683
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-10 01:10:06 +0530

    introduction file io

---
+++ lect15.py
@@ -0,0 +1,13 @@
+# file io basics
+"""
+"r" - Open file for reading
+"w" - open file for writing
+"x" - creates file if not exits
+"a" - Add more content to a file
+"D" - text mode
+"b" - binary mode
+"+" - read and write
+"""
+def func1():
+    """this is a doc function"""
+print(func1.__doc__)
